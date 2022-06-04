06a999958123a6222a7cc6798f57c571f171a77d
M.Kristóf <drzero123456@gmail.com>
2022-06-04 19:00:01 +0200

    Menu page content added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import "./style.css"
 
-console.log("test")
+const CONTENT_ELEM = document.getElementById("content");
+
+
+const generate_home = () => {
+
+    CONTENT_ELEM.innerHTML =  `<div class="navbar">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</div>
+
+<div class="main">
+    <img src="icon.png" alt="restaurant icon">
+    <h1 id="main-title">The United Stakes</h1>
+    <h2>We not only meat expectations we exceed them</h2>
+</div>
+
+<footer>@The Odin project</footer>`
+
+}
+
+
+
+
